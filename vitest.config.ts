8c2faaabb855268b3ba184ceef93598879008d9f
gary003 <gary.johnson003@gmail.com>
2026-02-02 14:28:58 +0700

    Light update of the test config & add sqlite db file in .gitignore

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,27 +1,32 @@
 import { defineConfig } from 'vitest/config'
 
+const filesToExclude = [
+  'src/v1/infrastructure/observability/**',
+  'src/v1/infrastructure/persistence/database/db_volume/**',
+  'src/v1/infrastructure/persistence/database/db_scripts/**',
+  './**/*dto.ts',
+  '**/*.db',
+  '**/db_file_volume/**',
+  '**/sqlite/**'
+]
+
+const globalSetup = process.env.UNIT_TEST == 'true' ? './tests/vitest.setup.unit.ts' : './tests/vitest.setup.ts'
+
+if (process.env.UNIT_TEST === 'true') {
+  filesToExclude.push('src/v1/infrastructure', 'src/v1/domain', 'src/v1/helpers')
+}
+
 export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    globalSetup: [process.env.UNIT_TEST === 'true' ? './tests/vitest.setup.unit.ts' : './tests/vitest.setup.ts'],
+    globalSetup,
     // This ensures Vitest looks for the right files regardless of ext
     include: ['./tests/**/*.spec.ts', './tests/**/*.test.ts'],
     coverage: {
       provider: 'v8',
       include: ['src/v1/**'],
-      exclude: [
-        process.env.UNIT_TEST === 'true' ? 'src/v1/infrastructure' : '',
-        process.env.UNIT_TEST === 'true' ? 'src/v1/domain' : '',
-        process.env.UNIT_TEST === 'true' ? 'src/v1/helpers' : '',
-        'src/v1/infrastructure/observability/**',
-        'src/v1/infrastructure/persistence/database/db_volume/**',
-        'src/v1/infrastructure/persistence/database/db_scripts/**',
-        './**/*dto.ts',
-        '**/*.db',
-        '**/db_file_volume/**',
-        '**/sqlite/**'
-      ]
+      exclude: filesToExclude
       // thresholds: {
       //   branches: 80,
       //   lines: 80,
